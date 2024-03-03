0d66d2e2a21be2c986493205391e39be92930803
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-03-03 22:01:52 +0300

    create window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "SDL2/SDL.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+int main(int argc, char *argv[])
+{
+    // returns zero on success else non-zero
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        printf("error initializing SDL: %s\n", SDL_GetError());
+    }
+    SDL_Window* win = SDL_CreateWindow("GAME",
+                                       SDL_WINDOWPOS_CENTERED,
+                                       SDL_WINDOWPOS_CENTERED,
+                                       1000, 1000, 0);
     return 0;
 }
+
