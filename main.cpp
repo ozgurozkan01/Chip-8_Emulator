ee6543660d82edee63e95c450d684f83f031f45e
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-03-03 22:07:06 +0300

    create window

--- main.cpp
+++ main.cpp
@@ -5,13 +5,15 @@
 int main(int argc, char *argv[])
 {
     // returns zero on success else non-zero
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
         printf("error initializing SDL: %s\n", SDL_GetError());
     }
-    SDL_Window* win = SDL_CreateWindow("GAME",
+
+    SDL_Window* win = SDL_CreateWindow("Chip-8 Emulator",
                                        SDL_WINDOWPOS_CENTERED,
                                        SDL_WINDOWPOS_CENTERED,
-                                       1000, 1000, 0);
+                                       720, 480, 0);
     return 0;
 }
 
