02c598ce6c7c9d4b45b1bf59193c3bc919462f91
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-03-03 22:01:46 +0300

    set up SDL2 lib

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/ozgur/CLionProjects/Chip-8_Emulator
+# Install script for directory: C:/Users/ozgur/GitHub/Chip-8_Emulator
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/ozgur/CLionProjects/Chip-8_Emulator/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/ozgur/GitHub/Chip-8_Emulator/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
