e55d13df58d5f9a70b07553edb99b72a883b216a
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-03-04 13:08:09 +0300

    add chip8 class to manage emulator

---
+++ src/Chip8.cpp
@@ -0,0 +1,15 @@
+//
+// Created by ozgur on 04/03/2024.
+//
+
+#include "Chip8.h"
+
+Chip8::Chip8()
+{
+    init();
+}
+
+void Chip8::init()
+{
+
+}
---
+++ src/Chip8.h
@@ -0,0 +1,19 @@
+//
+// Created by ozgur on 04/03/2024.
+//
+
+#ifndef CHIP_8_EMULATOR_CHIP8_H
+#define CHIP_8_EMULATOR_CHIP8_H
+
+
+class Chip8 {
+public:
+    Chip8();
+
+    void init();
+private:
+
+};
+
+
+#endif //CHIP_8_EMULATOR_CHIP8_H
