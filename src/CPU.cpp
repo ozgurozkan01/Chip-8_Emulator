361741ce78dcfa4778be613edb5fa9ade5ba373c
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-03-09 17:10:17 +0300

    add CPU class to manage operations will be performed by CPU via opcodes

---
+++ src/CPU.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ozgur on 09/03/2024.
+//
+
+#include "CPU.h"
---
+++ src/CPU.h
@@ -0,0 +1,14 @@
+//
+// Created by ozgur on 09/03/2024.
+//
+
+#ifndef CHIP_8_EMULATOR_CPU_H
+#define CHIP_8_EMULATOR_CPU_H
+
+
+class CPU {
+
+};
+
+
+#endif //CHIP_8_EMULATOR_CPU_H
