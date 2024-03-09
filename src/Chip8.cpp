b3b61f44f281afe70ade66a6366f67fa769f506d
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-03-09 11:07:09 +0300

    add RAM and SCREEN component

--- src/Chip8.cpp
+++ src/Chip8.cpp
@@ -3,14 +3,61 @@
 //
 
 #include "Chip8.h"
+#include "RAM.h"
+#include "Screen.h"
 
-Chip8::Chip8() :
+Chip8::Chip8(const char *romName) :
+    romName(romName),
+    memory(new RAM()),
+    screen(new Screen())
+{}
+
+bool Chip8::init()
 {
-    init();
+    if (!screen->init()) return false;
+
+    assignKeyMap();
+    return true;
 }
 
-void Chip8::init()
+void Chip8::assignKeyMap()
 {
+    /*  KEY MAP ON KEYBOARD
+     *  1 2 3 4
+     *  Q W E R
+     *  A S D F
+     *  Z X C V
+     */
 
+    keymap = new SDL_KeyCode[16]{
+            SDLK_q,
+            SDLK_w,
+            SDLK_e,
+            SDLK_r,
+            SDLK_a,
+            SDLK_s,
+            SDLK_d,
+            SDLK_f,
+            SDLK_z,
+            SDLK_x,
+            SDLK_c,
+            SDLK_v,
+            SDLK_1,
+            SDLK_2,
+            SDLK_3,
+            SDLK_4,
+    };
+}
+
+Chip8::~Chip8()
+{
+    delete [] keymap;
+    delete memory;
+    delete screen;
+}
+
+void Chip8::update() const
+{
+    screen->update();
 }
 
--- src/Chip8.h
+++ src/Chip8.h
@@ -6,20 +6,37 @@
 #define CHIP_8_EMULATOR_CHIP8_H
 
 #include <cstdint>
+#include "SDL2/SDL.h"
+
+class RAM;
+class Screen;
 
 class Chip8 {
 public:
-    Chip8();
 
-    void init();
+    // Components
+    RAM* memory;
+    Screen* screen;
+
+    Chip8(const char *romName);
+    ~Chip8();
+
+    bool init();
+    void update() const;
+
 private:
+    const char* romName; // Name of currently running rom
 
     uint16_t opcode; // operation code
     bool displayRefresh[64*32]; // (0xF00-0xFFF)
-    uint8_t pixels[64 * 32]; // pixel amount
+    uint8_t pixels[64*32]; // pixel amount
 
     uint8_t delayTimer;
     uint8_t soundTimer;
+
+    SDL_KeyCode* keymap; // Keys -> 0 - F
+
+    void assignKeyMap();
 };
 
 
