676163a81f954fe77c7bfc90af969ce5990332c2
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-03-09 15:57:51 +0300

    add readRom function to load ROM into memory in order to play on screen

--- src/ROM.h
+++ src/ROM.h
@@ -5,14 +5,17 @@
 #ifndef CHIP_8_EMULATOR_ROM_H
 #define CHIP_8_EMULATOR_ROM_H
 
+#include <fstream>
+#include <cstdint>
 
 class ROM {
 public:
     ROM(const char *romName);
-    ~ROM();
+    bool readRom(uint8_t* memory, const uint16_t& beginningPoint, const uint16_t& maxSize);
 private:
 
     const char* romName; // Name of currently running rom
+    std::ifstream romStream;
 };
 
 
