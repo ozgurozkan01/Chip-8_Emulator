6f14bb0d1f02b87020bb8ebbbb42b41bef4a7bf8
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-03-10 21:59:10 +0300

    add instruction struct, instruction variables and methods

--- src/CPU.h
+++ src/CPU.h
@@ -5,9 +5,31 @@
 #ifndef CHIP_8_EMULATOR_CPU_H
 #define CHIP_8_EMULATOR_CPU_H
 
+#include <cstdint>
+#include <bitset>
+
+struct Instructions
+{
+    // First nibble always says what kind of instruction it is.
+    // But other third can be different
+    uint16_t firstNibble;
+    uint16_t NNN;   // adress
+    uint8_t NN;     // 8 bit constant
+    uint8_t N;      // 4 bit constant
+    uint8_t X;      // 4 bit register identifier - always second
+    uint8_t Y;      // 4 bit refister identifier - always third
+};
 
 class CPU {
+public:
+    CPU();
 
+    void emulateInstructions(class RAM* ram);
+private:
+    Instructions instruction;
+    uint16_t opcode;
+    uint16_t PC; //  Program Counter which points at the current instruction in ram
+    uint16_t I; // Index Pointer which is used to point at locations in ram
 };
 
 
