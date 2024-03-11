034f65466a4b1a342183ad9879390f9b6f9fd9b3
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-03-12 00:09:39 +0300

    refactor code to make more readable

--- src/ROM.cpp
+++ src/ROM.cpp
@@ -2,29 +2,24 @@
 // Created by ozgur on 09/03/2024.
 //
 
-#include <iostream>
-
 #include "ROM.h"
 
-ROM::ROM(const char *romName) :
-        romName(romName) {}
+ROM::ROM(const char *romName) :  romName(romName) {}
 
 bool ROM::readRom(uint8_t* memory, const uint16_t& beginningPoint, const uint16_t& maxRamSize)
 {
     // ROM files are binary based files.
-    // Open Rom file
     romStream.open(romName, std::ios_base::binary);
 
     if (!romStream.is_open()) { return false; }
 
-    romStream.seekg(0, std::ios::end); // Find out file length
-    const long romSize = romStream.tellg(); // hold file length
-    romStream.seekg(std::ios::beg); // Get back to the beginning of file
+    romStream.seekg(0, std::ios::end);
+    const long romSize = romStream.tellg(); // ROM size
+    romStream.seekg(std::ios::beg);
 
     if (romSize > maxRamSize)
     {
         printf("ROM file : %s is bigger than usuable memory.\n", romName);
-        printf("ROM file size : %ld , Usuable memory size : &ld", romSize, maxRamSize);
         return false;
     }
 
