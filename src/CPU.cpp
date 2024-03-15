bb3ead8692d37c00fd4ee1c57f9ffc580ecea4f8
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-03-16 02:18:02 +0300

    fix some bugs and refactor code to make more readable

--- src/CPU.cpp
+++ src/CPU.cpp
@@ -44,18 +44,26 @@ void CPU::emulateInstructions(RAM* ram, Screen* screen, const bool* keymap, uint
             PC = instruction.NNN;
             break;
         case 0x2:
-            PC = instruction.NNN;
             ram->getStack().push(PC);
+            PC = instruction.NNN;
             break;
         case 0x3:
-            if (ram->getRegisters_V()[instruction.X] == instruction.NN) { PC += 2; }
+            if (ram->getRegisters_V()[instruction.X] == instruction.NN)
+            {
+                PC += 2;
+            }
             break;
         case 0x4:
-            if (ram->getRegisters_V()[instruction.X] != instruction.NN) { PC += 2; }
+            if (ram->getRegisters_V()[instruction.X] != instruction.NN)
+            {
+                PC += 2;
+            }
             break;
         case 0x5:
-            if (instruction.N == 0x0 &&
-                ram->getRegisters_V()[instruction.X] == ram->getRegisters_V()[instruction.Y]) { PC += 2; }
+            if (instruction.N == 0x0 && ram->getRegisters_V()[instruction.X] == ram->getRegisters_V()[instruction.Y])
+            {
+                PC += 2;
+            }
             break;
         case 0x6:
             ram->getRegisters_V()[instruction.X] = instruction.NN;
@@ -79,11 +87,11 @@ void CPU::emulateInstructions(RAM* ram, Screen* screen, const bool* keymap, uint
                     ram->getRegisters_V()[instruction.X] ^= ram->getRegisters_V()[instruction.Y];
                     break;
                 case 0x4:
-                    ram->getRegisters_V()[0x0F] = (uint16_t)(ram->getRegisters_V()[instruction.X] + ram->getRegisters_V()[instruction.Y]) > 255 ? 1 : 0;
+                    ram->getRegisters_V()[0x0F] = (uint16_t)(ram->getRegisters_V()[instruction.X] + ram->getRegisters_V()[instruction.Y]) > 0xFF;
                     ram->getRegisters_V()[instruction.X] += ram->getRegisters_V()[instruction.Y];
                     break;
                 case 0x5:
-                    ram->getRegisters_V()[0x0F] = ram->getRegisters_V()[instruction.X] >= ram->getRegisters_V()[instruction.Y] ? 1 : 0;
+                    ram->getRegisters_V()[0x0F] = ram->getRegisters_V()[instruction.X] >= ram->getRegisters_V()[instruction.Y];
                     ram->getRegisters_V()[instruction.X] -= ram->getRegisters_V()[instruction.Y];
                     break;
                 case 0x6:
@@ -91,20 +99,20 @@ void CPU::emulateInstructions(RAM* ram, Screen* screen, const bool* keymap, uint
                     ram->getRegisters_V()[instruction.X] >>= 1;
                     break;
                 case 0x7:
-                    ram->getRegisters_V()[0x0F] = ram->getRegisters_V()[instruction.Y] >= ram->getRegisters_V()[instruction.X]
-                    ?  1 : 0;
-                    ram->getRegisters_V()[instruction.X] =
-                            ram->getRegisters_V()[instruction.Y] - ram->getRegisters_V()[instruction.X];
+                    ram->getRegisters_V()[0x0F] = ram->getRegisters_V()[instruction.Y] >= ram->getRegisters_V()[instruction.X];
+                    ram->getRegisters_V()[instruction.X] = ram->getRegisters_V()[instruction.Y] - ram->getRegisters_V()[instruction.X];
                     break;
                 case 0xE:
-                    ram->getRegisters_V()[0x0F] = ((ram->getRegisters_V()[instruction.X]  & 0x80 ) >> 7);
+                    ram->getRegisters_V()[0x0F] = (ram->getRegisters_V()[instruction.X] >> 7) & 0x1;
                     ram->getRegisters_V()[instruction.X] <<= 1;
                     break;
             }
             break;
         case 0x9:
-            if (instruction.N == 0x0 &&
-                ram->getRegisters_V()[instruction.X] != ram->getRegisters_V()[instruction.Y]) { PC += 2; }
+            if (instruction.N == 0x0 && ram->getRegisters_V()[instruction.X] != ram->getRegisters_V()[instruction.Y])
+            {
+                PC += 2;
+            }
             break;
         case 0xA:
             I = instruction.NNN;
@@ -135,73 +143,61 @@ void CPU::emulateInstructions(RAM* ram, Screen* screen, const bool* keymap, uint
             }
             break;
         case 0x0F:
-            if (instruction.NN == 0x07)
-            {
-                ram->getRegisters_V()[instruction.X] = delayTimer;
-            }
-            else if (instruction.NN == 0x0A)
-            {
-                bool anyKeyPressed = false;
-                for (int i = 0; i < 16; ++i)
-                {
-                    if (keymap[i])
-                    {
-                        ram->getRegisters_V()[instruction.X] = i;
-                        anyKeyPressed = true;
-                        break;
-                    }
-                }
-
-                if (!anyKeyPressed)
-                {
+            switch (instruction.NN) {
+                case 0x07:
+                    ram->getRegisters_V()[instruction.X] = delayTimer;
+                    break;
+                case 0x0A:
                     PC -= 2;
-                }
-                break;
-            }
-            else if (instruction.NN == 0x15)
-            {
-                delayTimer = ram->getRegisters_V()[instruction.X];
-            }
-            else if (instruction.NN == 0x18)
-            {
-                soundTimer = ram->getRegisters_V()[instruction.X];
-            }
-            else if (instruction.NN == 0x1E)
-            {
-                if (I + ram->getRegisters_V()[instruction.X] > 0x0FFF)
-                {
-                    ram->getRegisters_V()[0xF] = 1;
-                }
+                    for (int i = 0; i < 16; ++i)
+                    {
+                        if(keymap[i])
+                        {
+                            ram->getRegisters_V()[instruction.X] = i;
+                            PC += 2;
+                            break;
+                        }
+                    }
+                    break;
+                case 0x15:
+                    delayTimer = ram->getRegisters_V()[instruction.X];
+                    break;
+                case 0x18:
+                    soundTimer = ram->getRegisters_V()[instruction.X];
+                    break;
+                case 0x1E:
+                    if (I + ram->getRegisters_V()[instruction.X] > 0x0FFF)
+                    {
+                        ram->getRegisters_V()[0xF] = 1;
+                    }
 
-                I += ram->getRegisters_V()[instruction.X];
-            }
-            else if (instruction.NN == 0x29)
-            {
-                I = (ram->getRegisters_V()[instruction.X] * 5);
-            }
-            else if (instruction.NN == 0x33)
-            {
-                uint8_t n = ram->getRegisters_V()[instruction.X];
-
-                for (int i = 2; i >= 0; i--)
-                {
-                    ram->getMemory()[I + i] = n % 10;
-                    n /= 10;
+                    I += ram->getRegisters_V()[instruction.X];
+                    break;
+                case 0x29:
+                    I = (ram->getRegisters_V()[instruction.X] * 5);
+                    break;
+                case 0x33:
+                { // This scope is in order to create local variables
+                    uint8_t n = ram->getRegisters_V()[instruction.X];
+                    for (int i = 2; i >= 0; i--)
+                    {
+                        ram->getMemory()[I + i] = n % 10;
+                        n /= 10;
+                    }
+                    break;
                 }
-            }
-            else if (instruction.NN == 0x55)
-            {
-                for (uint16_t i = 0; i <= instruction.X; ++i)
-                {
-                     ram->getMemory()[I + i] = ram->getRegisters_V()[i];
-                }
-            }
-            else if (instruction.NN == 0x65)
-            {
-                for (uint16_t i = 0; i <= instruction.X; ++i)
-                {
-                    ram->getRegisters_V()[i] = ram->getMemory()[I + i];
-                }
+                case 0x55:
+                    for (uint16_t i = 0; i <= instruction.X; ++i)
+                    {
+                        ram->getMemory()[I + i] = ram->getRegisters_V()[i];
+                    }
+                    break;
+                case 0x65:
+                    for (uint16_t i = 0; i <= instruction.X; ++i)
+                    {
+                        ram->getRegisters_V()[i] = ram->getMemory()[I + i];
+                    }
+                    break;
             }
             break;
         default:
