a5f9890048f3101f08d4e57673f9c86bcf5d8766
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-04-04 22:48:02 +0300

    delete redundant variables and functions

--- src/RAM.h
+++ src/RAM.h
@@ -11,26 +11,18 @@
 class RAM {
 public:
     RAM();
-    ~RAM();
-
-    uint16_t getMaxRamSize() const;
-    uint8_t* getMemory();
-    std::stack<uint16_t>& getStack();
-    uint8_t* getRegisters_V();
+    [[nodiscard]] uint16_t getMaxRamSize() const;
     [[nodiscard]] uint16_t getBeginningPoint() const;
+    std::stack<uint16_t>& getStack();
+    uint8_t* getMemory();
+    uint8_t* getRegisters_V();
 
 private:
     uint8_t memory[4096] = {0};
     uint8_t register_V[16]{0};
     std::stack<uint16_t> stack;
-    uint16_t *fontSet;
-
-    const uint32_t fontSize = 80;
     uint16_t ramBeginningPoint;
     uint16_t maxRamSize;
-
-    void init();
-    void loadFonts();
 };
 
 
