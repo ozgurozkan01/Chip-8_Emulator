17449323749f5683cec568bcc1b3453c8d51a237
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-05-21 04:26:09 +0300

    add beep sound implementation

--- src/Audio.cpp
+++ src/Audio.cpp
@@ -2,4 +2,63 @@
 // Created by ozgur on 5/20/2024.
 //
 
+#include <cstdio>
 #include "Audio.h"
+
+Audio::Audio() :
+    squareWaveFreq(440),
+    audioSampleRate(44100),
+    volume(3000)
+{
+}
+
+bool Audio::init()
+{
+    want.freq = 44100,
+    want.format = AUDIO_S16LSB,
+    want.channels = 1,
+    want.samples = 512,
+    want.callback = callback;
+    want.userdata = this;
+
+    deviceId = SDL_OpenAudioDevice(nullptr, 0, &want, &have, 0);
+
+    if (!deviceId)
+    {
+        printf("SDL Audio Device could not be created : %s", SDL_GetError());
+        return false;
+    }
+
+    if ((want.format != have.format) ||
+        (want.channels != have.channels))
+    {
+        SDL_Log("SDL Audio Spec could not be created ? %s\n", SDL_GetError());
+        return false;
+    }
+
+    return true; // Success
+}
+
+Audio::~Audio()
+{
+    SDL_CloseAudioDevice(deviceId);
+}
+
+void Audio::beep(int length) const
+{
+    SDL_PauseAudioDevice(deviceId, length);
+}
+
+void Audio::callback(void *userdata, uint8_t *streamBuffer, int length)
+{
+    auto * data = static_cast<Audio *>(userdata);
+    auto * stream = (int16_t *)streamBuffer;
+
+    static uint32_t index = 0;
+    const int32_t halfPeriod = (data->audioSampleRate / data->squareWaveFreq) / 2;
+
+    for (int i = 0; i < length / 2; i++)
+    {
+        stream[i] = ((index++ / halfPeriod) % 2) ? data->volume : -data->volume;
+    }
+ }
--- src/Audio.h
+++ src/Audio.h
@@ -5,10 +5,29 @@
 #ifndef CHIP_8_EMULATOR_AUDIO_H
 #define CHIP_8_EMULATOR_AUDIO_H
 
+#include <array>
+#include "SDL2/SDL.h"
 
 class Audio {
+public:
+    Audio();
 
+    ~Audio();
+
+    bool init();
+
+    void beep(int length) const;
+
+private:
+
+    static void callback(void *userdata, Uint8 *streamBuffer, int length);
+
+    SDL_AudioSpec want{}, have{};
+    SDL_AudioDeviceID deviceId{};
+
+    int32_t squareWaveFreq{};  // Frequency of square wave sound e.g. 440hz for middle A
+    int32_t audioSampleRate{};
+    int16_t volume;             // How loud or not is the sound};
 };
-
 
 #endif //CHIP_8_EMULATOR_AUDIO_H
