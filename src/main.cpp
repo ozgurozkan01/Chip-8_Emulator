f9b2ba147e89cd17a23324eb2ed344274d559cf4
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-06-07 13:40:22 +0300

    fix code logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 #include "SDL2/SDL.h"
 #include "Chip8.h"
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-    Chip8 chip8_Emulator("C:/Users/ozgur/GitHub/Chip-8_Emulator/ROMs/Brix [Andreas Gustafsson, 1990].ch8");
+    Chip8 chip8_Emulator("C:/Users/ozgur/GitHub/Chip-8_Emulator/ROMs/Pong [Paul Vervalin, 1990].ch8");
 
-    if (chip8_Emulator.init())
+    if (!chip8_Emulator.init())
+    {
+        printf("Emulator could not be initialized!\n");
+    }
+
+    while (chip8_Emulator.isRunning())
     {
         chip8_Emulator.update();
     }
